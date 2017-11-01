6a8bb82a848dfc0cb547da539f845ef414a5adf0
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-11-01 12:23:43 -1000

    initial commit

---
+++ react-kanban-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+
+// import './index.css';
+import App from './App';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
---
+++ react-kanban-app/src/reducers/index.js
@@ -0,0 +1,5 @@
+// import
+
+const initialState = {
+  //enter data here
+}
